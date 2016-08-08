84015166027415b5638543b573e30ed4c0cf6d96
cjus <Carlos.Justiniano@gmail.com>
2016-08-08 14:49:57 -0400

    Ability to read and write redis config data

--- hydra-cli.js
+++ hydra-cli.js
@@ -2,9 +2,15 @@
 'use strict';
 
 const fs = require('fs');
+const rl = require("readline");
 const moment = require('moment');
 
 class Program {
+  constructor() {
+    this.configData = null;
+    this.hydraConfig = null;
+  }
+
   displayHelp() {
     console.log('Usage: hydra-cli command [parameters]');
     console.log('');
@@ -16,11 +22,56 @@ class Program {
     console.log('  routes   display service API routes');
   }
 
+  _getUserHome() {
+    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+  }
+
   main() {
     if (process.argv.length < 3) {
       this.displayHelp();
       process.exit();
+      return;
     }
+
+    this.hydraConfig = `${this._getUserHome()}/.hydra-cli`;
+    fs.readFile(this.hydraConfig, 'utf8', (err, data) => {
+      if (!err) {
+        try {
+          this.configData = JSON.parse(data);
+        } catch (e) {
+          this.configData = null;
+        }
+      }
+
+      let command = process.argv[2];
+      let args = process.argv.slice(3);
+
+      switch (command) {
+        case 'config':
+          this.handleConfigCommand(args);
+          break;
+      }
+    });
+  }
+
+  handleConfigCommand(args) {
+    let prompts = rl.createInterface(process.stdin, process.stdout);
+    prompts.question('redisUrl: ', (redisUrl) => {
+      prompts.question('redisPort: ', (redisPort) => {
+        prompts.question('redisDb: ', (redisDb) => {
+          let data = this.configData || {};
+          data.redisUrl = redisUrl;
+          data.redisPort = redisPort;
+          data.redisDb = redisDb;
+          fs.writeFile(this.hydraConfig, JSON.stringify(data), (err) => {
+            if (err) {
+              console.log(err);
+            }
+            process.exit();
+          });
+        });
+      });
+    });
   }
 }
 
